2afd56ece84f850526f7a08bafec3c3295da8bb5
liyunpeng <liyunpeng@weidian.com>
2020-04-11 22:26:02 +0800

    auto update

--- uitester/base.py
+++ uitester/base.py
@@ -16,7 +16,7 @@ class WebDriver():
         self.logger = Logger("pywebdriver")
 
         options = webdriver.ChromeOptions()
-        options.add_argument('user-agent=""Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"')
+        options.add_argument('user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"')
 
         # self.start_server()
         if proxy_ip:
